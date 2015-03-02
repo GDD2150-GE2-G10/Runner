773e801de08b1267db8378d88c2df41e01a0a5fc
JaxFirehart <Kylecondo@gmail.com>
2015-03-02 14:12:48 -0700

    Implemented XML reading for game tuning and variable initialization

---
+++ Assets/Globals.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globals : MonoBehaviour 
+{
+	void Start () 
+    {
+        Debug.Log("Shape Bonus: " + Settings.PickupShapeBonus);
+        Debug.Log("Color Mult: " + Settings.PickupColorMult);
+        Debug.Log("Shape and Color Bonus: " + Settings.PickupShapeColorBonus);
+        Debug.Log("Shape and Color Mult: " + Settings.PickupShapeColorMult);
+	}
+}
---
+++ Assets/Settings.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Linq;
+using System.IO;
+
+public class Settings : MonoBehaviour
+{
+    public static int PickupShapeBonus;
+    public static float PickupColorMult;
+    public static float PickupShapeColorMult;
+    public static int PickupShapeColorBonus;
+
+    public TextAsset XMLDoc;
+
+    void Awake()
+    {
+        using (TextReader reader = new StringReader(XMLDoc.text))
+        {
+            XDocument pickupSettings = XDocument.Load(reader);
+            XElement root = pickupSettings.Element("Score");
+
+            PickupShapeBonus = int.Parse(root.Element("SameShapeBonus").Value);
+            PickupShapeColorBonus = int.Parse(root.Element("SameShapeColorBonus").Value);
+            PickupColorMult = float.Parse(root.Element("SameColorMult").Value);
+            PickupShapeColorMult = float.Parse(root.Element("SameShapeColorMult").Value);
+        }
+    }
+}
